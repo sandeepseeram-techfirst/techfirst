d0f2472ccdae75852818108197d6fa20997b7a56
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2022-07-02 16:17:06 +0530

    rust workshop

---
+++ rust_programming/july2022_week1/first_app/if_else_example.rs
@@ -0,0 +1,14 @@
+fn main()  
+{  
+  let a=3;  
+  let b=4;  
+  
+  if a>b  
+  {  
+     println!("a is greater than b");  
+  }  
+  else  
+   {  
+     println!("a is smaller than b");   
+   }  
+}  
