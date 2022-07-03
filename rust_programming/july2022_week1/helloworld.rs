7b3a5438fc3828593a74b824c68ef8d957e1510e
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2022-07-03 16:32:03 +0530

    rust workshop

---
+++ rust_programming/july2022_week1/helloworld.rs
@@ -0,0 +1,11 @@
+// This is a comment, and is ignored by the compiler
+
+// This code is editable 
+
+// This is the main function
+fn main() {
+
+    // Print text to the console
+    println!("Hello, World!");
+
+}
