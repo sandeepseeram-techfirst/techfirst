814b4d07e53f68db3d9947646a8f1cafa7f3009a
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2022-07-03 16:30:22 +0530

    rust workshop

---
+++ rust_programming/july2022_week1/first_app/else_if.rs
@@ -0,0 +1,15 @@
+fn main()  
+  
+ let num= -5;  
+ if num>0  
+ {  
+   println!("number is greater than 0");  
+ }  
+ else if num<0  
+ {  
+   println!("number is less than 0 ");  
+ }  
+ else  
+ {  
+   println!("number is not equal to 0");  
+ }  
