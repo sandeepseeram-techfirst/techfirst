31de3477e8eff111c5cd6290808b51acc3b47191
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2022-08-02 14:27:17 +0530

    added the server routes

--- python-flask/flask.py
+++ python-flask/flask.py
@@ -1,2 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
+
+"""
+Web server routes look up URIs to
+determine actions in a way similar to how routing on a network 
+performs a best path match to define traffic destinations
+
+"""
+
+@app.route('/newAPI')
+def new_api():
+    return 'Hello from within your new API'
+
+@app.route('/')
+def index():
+    return 'Response from Root URL' 
+
